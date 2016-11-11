93477440ca3f4611d68f1bc15f6bfc10f13e040b
handav <hannahdavis0220@gmail.com>
2016-11-11 16:04:22 +0100

    resetting first lesson

--- 1-tokenizing/index.js
+++ 1-tokenizing/index.js
@@ -1,6 +1,6 @@
 var natural = require('natural');
 var myString = "I'm surprised! I didn't know you could make it.";
 
-var tokenizer = new natural.RegexpTokenizer({pattern: /[!?.]/});
+var tokenizer = new natural.WordTokenizer();
 
 console.log(tokenizer.tokenize(myString));
